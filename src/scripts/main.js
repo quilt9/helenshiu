5e6d628ab0c48594624a958eb2518d0172fde075
Quilt9 <helen@quilt9.com>
2018-04-07 17:55:44 -0400

    Fixed the webfontload ReferenceError message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,28 @@
-var WebFont = require('webfontloader');
+/*
+var WebFont = require ('webfontloader');
 
 WebFont.load({
-	google: {
-	  families: ['Roboto', 'Roboto Mono']
-	}
+  google: {
+    families: ['Roboto', 'Roboto Mono'],
+  },
+  active: function(){
+    //when fonts loaded
+    console.log('fonts loaded');
+  }
 });
+WebFontConfig = {
+  custom: {
+    families: [
+      'Roboto', 'Roboto Mono'
+    ],
+    urls: [
+      'https://fonts.googleapis.com/css?family=Roboto|Roboto+Mono'
+    ],
+    // other options and settings
+	  active: function() {
+	    sessionStorage.fonts = true;
+	  }
+  },
+  timeout: 2000
+};
+*/
